a0188d58c15ff4a23965f646a0edd201680e66f7
Sebastião Victor <bysmuto0@gmail.com>
2025-02-10 11:32:04 -0300

    fullscreen videos

--- src/Components/YoutubeVideo.jsx
+++ src/Components/YoutubeVideo.jsx
@@ -1,11 +1,13 @@
- 
-import React from 'react';
+import React from "react";
 
-export default function Video({ videoId,extra }) {
+export default function Video({ videoId, extra }) {
   return (
     <iframe
+    
       className={`w-full h-full border-4 border-green-500  ${extra || ""}`}
       src={`https://www.youtube.com/embed/${videoId}`}
+      allowFullScreen
     />
+    
   );
 }
